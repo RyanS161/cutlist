854a88ba5c0d35a0c60b54953a604f5cf098689c
Ryan Slocum <ryanslo161@gmail.com>
2025-11-13 14:04:39 +0100

    Fixing bash bug

--- scripts/setup_finetuned_model.sh
+++ scripts/setup_finetuned_model.sh
@@ -1,17 +1,14 @@
-if [[ $# -ne 2 ]]; then
+if [[ $# -lt 1 || $# -gt 2 ]]; then
   echo "Usage: $0 <model_dir in model_output (e.g. 48059951/checkpoint-22000)> <rl_model_dir in model_output>"
   exit 1
 fi
 
 FINETUNED_MODEL="$1"
-
-if [[ -z "$2" ]]; then
-  RL_FINETUNED_MODEL="$2"
-fi
+RL_FINETUNED_MODEL="${2:-}"
 
 BASE_MODEL=$(grep -oP '/scratch/tmp\.[0-9]+\.rslocum/\K[a-zA-Z0-9._-]+' $SCRATCH/model_output/$FINETUNED_MODEL/adapter_config.json)
 
-rsync -av --exclude 'checkpoint*/' "$SCRATCH/model_output/$FINETUNED_MODEL/" "$TMPDIR/finetuned/"
+rsync -a --exclude 'checkpoint*/' "$SCRATCH/model_output/$FINETUNED_MODEL/" "$TMPDIR/finetuned/"
 
 # Replace references to previous tmpdir drive to current scratch drive
 grep -rIl -E '/scratch/tmp\.[0-9]+\.rslocum' $TMPDIR/finetuned | while read -r file; do
@@ -19,7 +16,7 @@ grep -rIl -E '/scratch/tmp\.[0-9]+\.rslocum' $TMPDIR/finetuned | while read -r f
 done
 
 if [[ -z "$RL_FINETUNED_MODEL" ]]; then
-  rsync -av --exclude 'checkpoint*/' "$SCRATCH/rl_grpo_cutlist/$FINETUNED_MODEL/" "$TMPDIR/rl_finetuned/"
+  rsync -a --exclude 'checkpoint*/' "$SCRATCH/rl_grpo_cutlist/$FINETUNED_MODEL/" "$TMPDIR/rl_finetuned/"
 
   grep -rIl -E '/scratch/tmp\.[0-9]+\.rslocum' $TMPDIR/rl_finetuned | while read -r file; do
     sed -i -E "s|/scratch/tmp\.[0-9]+\.rslocum|${TMPDIR}|g" "$file"
